4d3ab87b1790cfc53e05b81d02b2e973091984fc
gayathriuthamaraj <144335463+gayathriuthamaraj@users.noreply.github.com>
2026-04-18 06:33:26 +0530

    chore: add producer.go change alone

--- backend/internal/kafka/producer.go
+++ backend/internal/kafka/producer.go
@@ -52,13 +52,13 @@ type XDGSCRAMClient struct {
 }
 
 func (x *XDGSCRAMClient) Begin(userName, password, authzID string) error {
-       var err error
-       x.Client, err = x.HashGeneratorFcn.NewClient(userName, password, authzID)
-       if err != nil {
-	       return err
-       }
-       x.ClientConversation = x.Client.NewConversation()
-       return nil
+	var err error
+	x.Client, err = x.HashGeneratorFcn.NewClient(userName, password, authzID)
+	if err != nil {
+		return err
+	}
+	x.ClientConversation = x.Client.NewConversation()
+	return nil
 }
 
 func (x *XDGSCRAMClient) Step(challenge string) (string, error) {
